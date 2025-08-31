fb5a4af2f32b6398c421f425c89a8334d323ca25
Boon <boonsvj2016@gmail.com>
2025-08-31 02:43:44 +0100

    same

--- api/translate.ts
+++ api/translate.ts
@@ -41,7 +41,7 @@ export async function POST(request: Request) {
     let end = new Date().getMilliseconds()
 
     response.timetook = end;
-    return new Response(JSON.stringify(response), { headers: { 'Access-Control-Allow-Origin': ALLOWED_ORIGIN },})
+    return new Response(JSON.stringify(response), { status: 200,  headers: { 'Access-Control-Allow-Origin': ALLOWED_ORIGIN },})
   } catch (error) {
     return new Response(JSON.stringify({
       error: 'Failed to parse request body',
